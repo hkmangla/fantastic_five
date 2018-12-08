459f4e3cab90bfc5eb882c2f5927f524f64ddc2c
hkmangla <hemantmangla78@gmail.com>
2018-12-08 12:25:19 +0530

    feat: Add angular front end and nodemon

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express'),
     app = express();
 
 const port = 8088;
+
+app.get('/', (req, res) => {
+    res.send("Home Page");
+});
+
 app.listen(port, () => {
     console.log("App started at port", port);
 })
