6b38037de478ee329c5a982c9b9f9dd201558859
hkmangla <hemantmangla78@gmail.com>
2018-12-08 13:42:30 +0530

    Setup front end and back end code and commands

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-var express = require('express'),
-    app = express();
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
+      path = require('path');
+
+var app = express();
 
 const port = 8088;
 
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res) => {
     res.send("Home Page");
 });
