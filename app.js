2afbc3030585c15131e86fdb76e31a36181f5a44
hkmangla <hemantmangla78@gmail.com>
2018-12-08 22:30:34 +0530

    Setup basic client side routing

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.use('/events', eventsRouter);
 app.get('/', (req, res) => {
     res.send("Home Page");
 });
+
 
 app.listen(port, () => {
     console.log("App started at port", port);
