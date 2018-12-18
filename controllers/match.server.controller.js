f5b1fa90674c17490b30ad002e6e6765f5752365
saiiiyan <kunalaggarwal58@gmail.com>
2018-12-18 21:22:25 +0530

    Parsed api data in Match controller

--- controllers/match.server.controller.js
+++ controllers/match.server.controller.js
@@ -3,15 +3,26 @@ const {HLTV} = require('hltv');
 module.exports.getMatches = function (req, resp) {
 	console.log('yes');
 	HLTV.getMatches().then( res => {
-		console.log(res);
-    resp.send(JSON.stringify(res));
+		//console.log(res);
+		// if(err)console.log(err);
+		var resp2 = [];
+		res.forEach(element => {
+			var temp = { team1 : element.team1,
+			team2 : element.team2,
+			};
+			resp2.push(temp);
+		});
+    resp.send(JSON.stringify(resp2));
 	});
 }
 module.exports.getMatch = function (req, resp) {
-    console.log('yes');
     var id = req.params.id;
 	HLTV.getMatch({id : id}).then( res => {
-		console.log(res);
+		//console.log(res);
+	//if(err)console.log(err);
+	var resp2 = {team1 : element.team1,
+		team2 : element.team2,};
+	
     resp.send(JSON.stringify(res));
 	});
 }
