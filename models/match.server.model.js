89568b418ed25b2e66138ccf869ed318807d7593
saiiiyan <kunalaggarwal58@gmail.com>
2018-12-18 20:53:45 +0530

    Updated Match Schema

--- models/match.server.model.js
+++ models/match.server.model.js
@@ -1,19 +1,37 @@
 var mongoose = require("mongoose"),
 		Team = require("team.server.model.js"),
 		matchSchema = new mongoose.Schema({
-			team1 : [
-				type : mongoose.Schema.Types.ObjectId,
-				ref  : "Team"
-		],
-		team2 : [
-				type : mongoose.Schema.Types.ObjectId,
-				ref  : "Team"
-		],
+			team1 : {
+				name : String,
+				id : Number
+			},
+		team2 : {
+			name : String,
+			id : Number
+		},
 
 		bettingLeagues : [
+			{
 				type : mongoose.Schema.Types.ObjectId,
 				ref  : "League"
+			}
+				
 		],
+		players : {
+			team1 : [
+				{
+					name: String,
+					id 	: Number
+				}
+			],
+			team2 : [
+				{
+					name: String,
+					id : Number
+				}
+			]
+
+		},
 
 		matchStatus : String,
 		winner : String,
